bfe6ad9b026ab0ec4fe181c4faf4058d9804aad3
bs5555 <faa.balazs@gmail.com>
2023-03-03 11:59:28 -0600

    Traffic sign factory READY

---
+++ traffic_sign_factory/res/rat.js
@@ -0,0 +1,16 @@
+var rats =
+[
+{
+'Better' :      ['Worser','Greater','Smaller'],
+'be' :          ['be','not to be','having fun'],
+'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+'than' :        ['than']
+},
+{
+'You' :      ['You','I','Grandma','Curiosity'],
+'killed' :   ['killed','revived','painted','made to purr'],
+'a Rat' :    ['a Rat','a Samurai','the Cat','drivers','someone']
+}
+
+];
